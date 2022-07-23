d07e25a007353930a4bca869edfbe591bd5d5646
elesis-sieghart <elesis@github.com>
2022-07-23 17:34:24 +0900

    test: test kakao login

--- routers/user-router.js
+++ routers/user-router.js
@@ -97,13 +97,18 @@ userRouter.post(
 //  카카오 서버를 통해 카카오 로그인을 하게 되면, 다음 라우터로 요청한다.
 userRouter.get('/kakao', passport.authenticate('kakao'));
 
+//? 위에서 카카오 서버 로그인이 되면, 카카오 redirect url 설정에 따라 이쪽 라우터로 오게 된다.
 userRouter.get(
   '/kakao/callback',
+  //? 그리고 passport 로그인 전략에 의해 kakaoStrategy로 가서 카카오계정 정보와 DB를 비교해서 회원가입시키거나 로그인 처리하게 한다.
   passport.authenticate('kakao', {
-    failureRedirect: '/',
+    failureMessage: 'login failure',
+    // failureRedirect: '/n', // kakaoStrategy에서 실패한다면 실행
   }),
+  // kakaoStrategy에서 성공한다면 콜백 실행
   async function (req, res, next) {
-    res.redirect('/');
+    // res.redirect('/');
+    return res.status(201).send({ message: 'login success' });
   }
 );
 
