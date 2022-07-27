e1f76787552e6fb1f7149161ea236d57c547aea7
elesis-sieghart <elesis@github.com>
2022-07-27 10:24:01 +0900

    fix: kakao login

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ import { specs } from './swagger.js';
 const app = express();
 
 // CORS 에러 방지
-app.use(cors());
+app.use(
+  cors({
+    origin: '*', // 출처 허용 옵션
+    credential: 'true', // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
+  })
+);
 
 // passport.js LocalStrategy 사용하는 경우 (+session)
 app.use(session({ secret: process.env.COOKIE_SECRET }));
